41fcd39aa84798316c4f8075e0b0d2d81b9470c9
Aisulu Shahabdin <aisulu_sh_98@mail.ru>
2023-12-24 23:26:27 +0600

    HW 25.1

---
+++ study/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_staff and request.user == request.get_object().author:
+            return True
+        return False
+
+
+class IsAuthor(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_staff and request.user == request.get_object().author:
+            return True
+        return False
